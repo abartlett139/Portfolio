e5cd65b6a32de63d5b2b8d53c9d641f003eaa4bc
Amy Katherine Bartlett <abartlett139@gmail.com>
2023-08-09 22:38:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ LIGHT1.intensity = .7;
 LIGHT2 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(-1, -1, 0), MAIN_SCENE);
 LIGHT2.intensity = .3;
 
-// MAIN_SCENE.clearColor = new BABYLON.Color3(.15, .15, .15);
-// H = MAIN_SCENE.createDefaultEnvironment();
-// H.setMainColor(new BABYLON.Color3(0.0, 0.1, 0.07));
-
-// MAIN_SCENE.ambientColor = new BABYLON.Color3(0, 1, 0);
-
 DOME = new BABYLON.PhotoDome(
     "background",
     "rs/photodome.jpg",
@@ -34,12 +28,6 @@ DOME.position.y = -10;
 
 initSceneExtras();
 
-//currently using the position vector to place a flower doesn't work
-//var test = new LSystem("Lily", new BABYLON.Vector3(0, 0, 0));
-
-
-
 ENGINE.runRenderLoop(function(){
     MAIN_SCENE.render();
-    ENGINE.wipeCaches(true);
 });
